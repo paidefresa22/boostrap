7e4414a2b77a79e0d1eb6f3365adf83adc5da2ee
paidefresa22 <pauceleste41@gmail.com>
2024-11-22 11:28:02 -0400

    arrays

---
+++ js/js/arrays.js
@@ -0,0 +1,21 @@
+//array es una coleccion de tipos de datos
+//var nombre = 'jenny';
+
+//var alumnos = ['Daniela','Paula','Gabriel','Carlos'];
+
+//console.log(alumnos);
+
+//console.log(alumnos[1]);//asi imprimo una posicion en especifico de mi arreglo entendiendo que lo qie
+//esta entre los corchetes es el indice
+
+//console.log(alumnos.length);//para conocer el largo de mi arreglo
+
+const frutas = ['Durazno','Fresa','Piña','Patilla','Naranja'];
+for (var i=0; i<frutas.length; i++){
+    console.log(i)
+    console.log(frutas[i]);
+}
+
+const frutas2 = ['mango', 'peras'];
+
+console.log(...frutas2,frutas);
