718fd4429adcd6a7c1b718e630858d23f2087313
paidefresa22 <pauceleste41@gmail.com>
2024-11-22 11:29:11 -0400

    parametros

---
+++ js/13-parametros-funciones.js
@@ -0,0 +1,22 @@
+//METODO Y  FUNCIONES
+
+let n1= 15;
+let n2= 20;
+let n3="55";
+
+console.log(parseInt(n1));//esto es una funcion porque coloco el nombre de la funcion acpmpañada 
+console.log(n1.toString());// esto es un metodo va despues de una variable
+console.log(parseInt(n3));// este metodo me convierte en numeros un string
+
+//INCREMENTADORES EN JAVA SVRIPT
+var b= 15;
+var c= "15";
+//console.log(b++);
+//console.log(b++);
+//console.log(--b);
+console.log(++b);
+
+var compruebo= b != c;//valido que sean distinto tanto en valor como en tipo de datos
+var compruebo2= b ===c;//comprobar que dos variables tengan el mismo valor cpmp que..
+console.log(compruebo);
+console.log(compruebo2);
